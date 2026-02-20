877a53606bcb6039703c1cf368a01facf0bda5b4
CJMoses1318 <cjmoses1318@gmail.com>
2026-02-20 08:21:05 -0800

    first commit and firebase

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import HomeScreen from './HomeScreen';
+import Nav from './Nav';
+
+function App() {
+  return (
+    <div className="app">
+      <HomeScreen />
+      <Nav />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBF0Kx4fgSDRlIkvsmN-0rvG_AK3WB-yPo",
+  authDomain: "fes-netflix-clone.firebaseapp.com",
+  projectId: "fes-netflix-clone",
+  storageBucket: "fes-netflix-clone.firebasestorage.app",
+  messagingSenderId: "778331653168",
+  appId: "1:778331653168:web:d384d36b654d38930699f5",
+  measurementId: "G-FVS2E37GTG"
+};
