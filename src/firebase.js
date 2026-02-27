0c6180fa072a62153ed4ccbca35a943e9c2b23f4
CJMoses1318 <cjmoses1318@gmail.com>
2026-02-26 22:27:00 -0800

    working on stripe

--- src/firebase.js
+++ src/firebase.js
@@ -1,40 +1,20 @@
 import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
 import "firebase/compat/auth";
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
-
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBF0Kx4fgSDRlIkvsmN-0rvG_AK3WB-yPo",
-  authDomain: "fes-netflix-clone.firebaseapp.com",
-  projectId: "fes-netflix-clone",
-  storageBucket: "fes-netflix-clone.firebasestorage.app",
-  messagingSenderId: "778331653168",
-  appId: "1:778331653168:web:d384d36b654d38930699f5",
-  measurementId: "G-FVS2E37GTG"
+  apiKey: "AIzaSyBOrIM_s6vgX-OWQZJWIrgwxvnTU2HLV0g",
+  authDomain: "clone-netflix-498f9.firebaseapp.com",
+  projectId: "clone-netflix-498f9",
+  storageBucket: "clone-netflix-498f9.firebasestorage.app",
+  messagingSenderId: "544098043489",
+  appId: "1:544098043489:web:6f8e591675a5e305b70c20",
+  measurementId: "G-8Y1S5RH6RN"
 };
-
-// async function getCustomers() {
-//   const customersCollection = collection(db, 'customers');
-//   const customerSnapshot = await getDocs(customersCollection);
-//   const customerList = customerSnapshot.docs.map(doc => ({
-//     id: doc.id,
-//     ...doc.data()
-//   }));
-
-//   console.log(customerList); // This will log the list of customers
-// }
-
-// // Call the function to fetch customers
-// getCustomers();
-
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
-const auth = firebaseApp.auth();
-
-
+const auth = firebase.auth();
 
 export { auth };
 export default db;
